fcb4b71c50088b634a79eee874e3b72edb741cec
mohammadreza-davarzani-82 <mohammaddavarzani1998@gmail.com>
2023-08-22 16:49:00 +0330

    handle add todo and add modal in Column

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,22 +1,33 @@
 import { useStore } from "../../zustand/store";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Task from "../Task/Task";
 import "./Column.css";
-
+import Modal from "../Modal/Modal";
 const Column = ({ state }) => {
+  const [value , setValue] = useState("")
+  const [open , setOpen] = useState("")
+
   const tasks = useStore((store) =>
     store.tasks.filter((task) => task.state === state)
   );
-  const addTask = useStore((store) => store.addTask)
+  const addTask = useStore((store) => store.addTask(value,state))
+
+ const  handleChange = (e) =>{
+    setValue(e.target.value)
+  }
   return (
     <div className="column">
       <header className="title-wrapp">
         <p>{state}</p>
-        <button onClick={()=>addTask("learn zustand" , state)}>Add</button>
+        <button onClick={()=>setOpen(true)}>Add</button>
       </header>
       {tasks.map((task) => (
         <Task title={task.title} key={task.title} />
       ))}
+      <Modal handleChange={handleChange} showModal={open} handleClose={setOpen}>
+
+      </Modal>
     </div>
   );
 };
